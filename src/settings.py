46e350ef561ddfc8498db9b93fd6ca6bc92aa4ac
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-16 20:48:52 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -41,7 +41,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://github.com/cwfid/dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 295363
+PREVIEW_IMAGE_ID: int = 295350
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/cwfid"
